22e4739a20fd7e902305cb337fae7b1e81e84b05
Beckers <BeckersL@r.bafg.de>
2022-03-03 08:31:17 +0100

    Pfad geändert

--- tests/ufid_alignment/liza_get_data.R
+++ tests/ufid_alignment/liza_get_data.R
@@ -1,7 +1,7 @@
 library(ntsportal)
 index <- "g2_nts_expn"
 path_ufid_db <- "~/projects/ufid/tests/ufid1.sqlite"
-config_path <- "~/projects/config.yml"
+config_path <- "~/Projects/ntsportal/tests/ufid_alignment/config.yml"
 ec <- config::get("elastic_connect", file = config_path)
 escon <- elastic::connect(host = '10.140.73.204', user=ec$user, pwd=ec$pwd)
 
