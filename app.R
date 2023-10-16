aef723cf27756fffa64d222d8b59caa0c15dfdf4
Georg Ebert <georgebert@mbgebert-2.local>
2023-10-16 14:49:38 +0200

    run checked new code in request ui

--- app.R
+++ app.R
@@ -4,5 +4,3 @@
 #reactlog::reactlog_enable() # enables the reactlog for debugging
 shiny::shinyApp(ui, server)
 
-#http://localhost:8787/p/e5855dc8/
-#http://localhost:8787/p/e5855dc8/R/_Home/include/bfg-graphik.png
