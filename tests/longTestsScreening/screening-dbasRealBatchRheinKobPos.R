d6fe0098fa84bf1d95cb6e73248990909d3012eb
jewell <jewell@bafg.de>
2024-11-25 12:53:12 +0100

    added test function rhein

---
+++ tests/longTestsScreening/screening-dbasRealBatchRheinKobPos.R
@@ -0,0 +1,10 @@
+
+
+# nohup Rscript tests/longTestsScreening/screening-dbasRealBatchRheinKobPos.R &> tests/longTestsScreening/testResults/rheinKobPos.log &
+
+tempSaveDir <- "tests/longTestsScreening/testResults/"
+index <- "ntsp_msrawfiles"
+batchDirectory <- "/srv/cifs-mounts/g2/G/G2/HRMS/Messdaten/koblenz/wasser/2021/202105/pos/"
+ntsportal::connectNtsportal()
+ntsportal::dbaScreeningSelectedBatches(index, batchDirectory, tempSaveDir)
+warnings()
