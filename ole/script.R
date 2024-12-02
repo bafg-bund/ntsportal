434f253a82bc8f0c92ee71df0443be1fda2b8926
jewell <jewell@bafg.de>
2024-12-02 14:15:49 +0100

    deleted "ole"

--- ole/script.R
+++
@@ -1,4 +0,0 @@
-json_path = "/home/lessmann/ntsgz/db/ntsp/unit_tests/record_files/test"
-
-
-ingestJson(json_path)
--- ole/script_credentials.R
+++
@@ -1,9 +0,0 @@
-# install.packages('reticulate')
-# reticulate::py_install('keyring')
-# reticulate::py_install('secretstorage')
-# reticulate::py_install('dbus-python')
-
-reticulate::source_python("~/projects/ntsportal/tests/ole/get_credentials.py")
-
-
-cred = get_credentials()
