b9015d67ae78d92113575a3a44696e4963e0235f
jewell <jewell@bafg.de>
2025-04-15 10:23:02 +0200

    added installation tests

--- tests/manualInstallationTests/installationOnPvil-rr.R
+++ tests/manualInstallationTests/installationOnPvil-rr.R
@@ -9,11 +9,23 @@ rstudioapi::openProject(file.path(tmpDir, "ntsportal"))
 # when in new project, run
 renv::restore()  # activate project
 renv::install("bafg-bund/ntsworkflow")
+
+# create new python virtual env
+reticulate::virtualenv_create("testvenv", requirements = "requirements.txt")
+oldPythonEnv <- Sys.getenv("RETICULATE_PYTHON")
+newPythonEnv <- "~/.virtualenvs/testvenv/bin/python"
+Sys.setenv(RETICULATE_PYTHON = newPythonEnv)
+reticulate::use_virtualenv("~/.virtualenvs/testvenv")
+stopifnot(grepl("testvenv", reticulate::py_config()$python))
+
 devtools::load_all()
 testthat::test_file("tests/testthat/test-screening-fileScanning.R")
+testthat::test_file("tests/testthat/test-connectionToElasticSearch.R")
+
 
 # switch back to original project, cleanup
 
+reticulate::virtualenv_remove("~/.virtualenvs/testvenv")
 tmpDir <- "~/temp_installation_ntsportal"
 file.remove(list.files(tmpDir, recursive = T))
 file.remove(tmpDir)
